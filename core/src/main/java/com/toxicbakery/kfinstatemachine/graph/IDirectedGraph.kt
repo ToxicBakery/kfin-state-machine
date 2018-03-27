c6e30c95914af4855e836a9b030cab789076551e
Ian Thomas <toxicbakery@gmail.com>
2018-03-26 20:15:23 -0400

    Added missing documentation to the core module

--- core/src/main/java/com/toxicbakery/kfinstatemachine/graph/IDirectedGraph.kt
+++ core/src/main/java/com/toxicbakery/kfinstatemachine/graph/IDirectedGraph.kt
@@ -1,12 +1,28 @@
 package com.toxicbakery.kfinstatemachine.graph
 
+/**
+ * Directed graph that defines nodes and edges.
+ */
 interface IDirectedGraph<N, out L> {
+
+    /**
+     * All of the nodes defined in the graph.
+     */
     val nodes: Set<N>
 
+    /**
+     * All of the edges defined in the graph.
+     */
     val edges: Set<GraphEdge<N, L>>
 
+    /**
+     * Edges of the graph grouped by their left node.
+     */
     val mappedEdges: Map<N, List<GraphEdge<N, L>>>
 
+    /**
+     * Transitions available to a specific node.
+     */
     fun nodeTransitions(node: N): Set<L>
 
 }
