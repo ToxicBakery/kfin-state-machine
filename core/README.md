753851f706ddf9d93afb30ee163c33f7781b1109
toxicbakery <toxicbakery@gmail.com>
2018-12-25 00:52:29 -0500

    Conversion to kotlin multiplatform

--- core/README.md
+++ core/README.md
@@ -1,11 +1,32 @@
-# Core
+## Core
 Core library providing a base state machine implementation and graph features for building machines.
 
-## Sample Usage
+### Sample Usage
 [See Main README](../)
 
-## Install
+### Install
 Includes base state machine implementation
 ```groovy
 compile "com.ToxicBakery.kfinstatemachine:core:2.+"
+```
+
+## RxJava
+Rx bindings for listening to state changes and transition events of a machine.
+
+### Sample Usage
+```kotlin
+val stateMachine = StateMachine(
+                Potential,
+                transition(Potential, Release::class, Kinetic),
+                transition(Kinetic, Store::class, Potential))
+                .let { stateMachine -> RxStateMachine(stateMachine) }
+
+// Listen for state changes
+stateMachine.observable.subscribe {...}
+```
+
+### Install
+Includes [Core](core) dependency
+```groovy
+compile "com.ToxicBakery.kfinstatemachine:rx:2.+"
 ```
---
+++ samples/dungeon/src/test/java/com/toxicbakery/sample/dungeon/DirectedGraphKtTest.kt
@@ -0,0 +1,38 @@
+package com.toxicbakery.sample.dungeon
+
+import com.toxicbakery.kfinstatemachine.TransitionRule
+import com.toxicbakery.kfinstatemachine.graph.DirectedGraph
+import com.toxicbakery.sample.dungeon.DirectedGraphKtTest.Action.Release
+import com.toxicbakery.sample.dungeon.DirectedGraphKtTest.Action.Store
+import com.toxicbakery.sample.dungeon.DirectedGraphKtTest.Energy.Kinetic
+import com.toxicbakery.sample.dungeon.DirectedGraphKtTest.Energy.Potential
+import org.junit.Test
+import kotlin.reflect.KClass
+
+class DirectedGraphKtTest {
+
+    @Test
+    fun getToTransitionRules() {
+        DirectedGraph(
+                mapOf(
+                        Potential to mapOf(Release::class to Kinetic),
+                        Kinetic to mapOf(Store::class to Potential)
+                ))
+                .toTransitionRules
+                .also { transitions: List<TransitionRule<Energy, out KClass<out Action>>> ->
+                    transitions.first { it.oldState == Potential && it.newState == Kinetic }
+                    transitions.first { it.oldState == Kinetic && it.newState == Potential }
+                }
+    }
+
+    enum class Energy {
+        Potential,
+        Kinetic
+    }
+
+    enum class Action {
+        Release,
+        Store
+    }
+
+}
